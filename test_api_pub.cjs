a838e7bb7088b5a4b2913b1d62a7e598ee7b46e9
Thamaraiselvan <thamarailucid@gmail.com>
2026-09-09 18:37:06 +0530

    chore: remove unused scratch, alter, test, and generator scripts to clean codebase

--- test_api_pub.cjs
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-http.get('http://localhost:5000/api/v1/MarketingSetting/GetPublicMarketingSettings', (resp) => {
-  let data = '';
-  resp.on('data', (chunk) => { data += chunk; });
-  resp.on('end', () => { console.log(data); });
-}).on('error', (err) => { console.log('Error: ' + err.message); });
